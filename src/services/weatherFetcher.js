e38423174049b1aec54d864921a0adebb95d2d38
Merce Casamitjana <casamitjana.agraz@gmail.com>
2021-04-06 23:14:13 +0200

    fix http secure openapi

--- src/services/weatherFetcher.js
+++ src/services/weatherFetcher.js
@@ -3,7 +3,7 @@ import WeatherLocation from './weatherLocation';
 import openweathermapConfig from './api.config';
 
 async function weatherFetcher(cityName, units) {
-  return axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&&lang=sp&units=${units}&appid=${openweathermapConfig.appId}`)
+  return axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&&lang=sp&units=${units}&appid=${openweathermapConfig.appId}`)
     .then((res) => new WeatherLocation(
       res.data.name,
       res.data.coord,
